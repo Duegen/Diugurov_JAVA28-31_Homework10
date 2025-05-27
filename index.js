435f3a9ef81780a4c13a3c5a36a33733a88a1d6b
Duegen <nikitadyugurov@gmail.com>
2025-05-27 19:40:14 +0300

    Version 1.02

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const info = document.querySelectorAll('.left p')
 const last = document.querySelectorAll('.right p');
 const right = document.querySelector('.right');
 
-btn_add.onclick = function (){
+btn_add.onclick = function () {
     let random = new Array(5);
-    for (let i = 0; i < 5; i++){
-        random[i] = Math.floor(Math.random()*36) +1;
+    for (let i = 0; i < 5; i++) {
+        random[i] = Math.floor(Math.random() * 36) + 1;
     }
     let random_str = random.toString()
     print_stat(random_str)
@@ -25,57 +25,61 @@ btn_clear.onclick = function () {
     }
 }
 
-function min(massive){
+function min(massive) {
     let min = Number.MAX_SAFE_INTEGER;
-    for(let i = 0; i < massive.length; i++)
-        if(massive[i] < min) min = massive[i];
+    for (let i = 0; i < massive.length; i++)
+        if (massive[i] < min) min = massive[i];
     return min;
 }
-function max(massive){
+
+function max(massive) {
     let max = Number.MIN_SAFE_INTEGER;
-    for(let i = 0; i < massive.length; i++)
-        if(massive[i] > max) max = massive[i];
+    for (let i = 0; i < massive.length; i++)
+        if (massive[i] > max) max = massive[i];
     return max;
 }
-function avg(massive){
+
+function avg(massive) {
     let avrg = 0;
-    for(let i = 0; i < massive.length; i++)
+    for (let i = 0; i < massive.length; i++)
         avrg += massive[i];
-    return avrg/massive.length;
+    return avrg / massive.length;
 }
-function Statisctics(min, max, avrg, massive){
+
+function Statisctics(min, max, avrg, massive) {
     const stat = {
-        'Min value' : min(massive),
+        'Min value': min(massive),
         'Max value': max(massive),
         'Average': avrg(massive)
     }
     return stat;
 }
-function print_stat(random_str){
+
+function print_stat(random_str) {
     info[0].innerHTML = info[0].getAttribute('data-title') + random_str
     let random = random_str.split(',').map(Number);
     for (let i = 1; i < 4; i++) {
-        info[i].innerHTML = info[i].getAttribute('data-title') + Object.values(Statisctics(min, max, avg, random))[i-1];
+        info[i].innerHTML = info[i].getAttribute('data-title') + Object.values(Statisctics(min, max, avg, random))[i - 1];
     }
 
 }
 
 function add_number(random_str) {
     let index = Number(right.getAttribute('data-index'));
-    // if (index === 5) {
-        for (let i = index; i > 0; i--) {
-            last[i].innerHTML = last[i - 1].innerHTML;
-            last[i].onclick = function () {
-                print_stat(last[i].innerHTML);
-            }
-        }
-        last[0].innerHTML = random_str;
-        last[0].onclick = function () {
-            print_stat(last[0].innerHTML);
-        }
-    last[index].style.display = "block";
-        if(index < last.length - 1){
 
-            right.setAttribute('data-index', (index + 1).toString());
+    for (let i = index; i > 0; i--) {
+        last[i].innerHTML = last[i - 1].innerHTML;
+        last[i].onclick = function () {
+            print_stat(last[i].innerHTML);
         }
+    }
+    last[0].innerHTML = random_str;
+    last[0].onclick = function () {
+        print_stat(last[0].innerHTML);
+    }
+    last[index].style.display = "block";
+    if (index < last.length - 1) {
+
+        right.setAttribute('data-index', (index + 1).toString());
+    }
 }
